56aaf8f71f5f4f99eef2249e6ace70049895b399
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-11-19 18:19:28 +0900

    [Add]홈화면 레이아웃 완성

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,87 +1,110 @@
-// Main.js 파일
-
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/images/logo.svg";
 import {
   Container,
   Image,
-  Wrapper,
-  LinkButton,
+  LinkWrapper,
+  ThinLink,
+  BoldLink,
   Button,
   SignupWrapper,
   SignUpInput,
+  LogoImage,
+  ErrorMessage,
 } from "./styled";
-import signupPwd from "../../assets/images/signupPwd.svg";
+import User from "../../assets/images/signupUser.svg";
+import BackList from "../../assets/images/banklist.svg";
+import AccountNum from "../../assets/images/account.svg";
+import Group from "../../assets/images/sosock.svg";
+import ID from "../../assets/images/saupja.svg";
 
 const Main = () => {
+  const [name, setName] = useState("");
+  const [financialInstitution, setFinancialInstitution] = useState("");
+  const [transactionNumber, setTransactionNumber] = useState("");
+  const [affiliation, setAffiliation] = useState("");
+  const [userNumber, setUserNumber] = useState("");
+  const [showError, setShowError] = useState(false);
+
   const handleSearch = () => {
-    console.log('조회 버튼이 클릭되었습니다.');
+    if (!name || !financialInstitution || !transactionNumber) {
+      setShowError(true);
+    } else {
+      setShowError(false);
+      console.log('조회 버튼이 클릭되었습니다.');
+      // 여기에 조회 로직 추가
+    }
   };
 
   return (
     <Container>
-      <Image src={Logo} alt="로고이미지" />
+      <LogoImage src={Logo} alt="로고이미지" className="logo" />
 
       <SignupWrapper>
-        <Image src={signupPwd} alt="비밀번호이미지" />
+        <Image src={User} alt="이름이미지" />
         <SignUpInput
           id="name"
           type="text"
-          placeholder="이름을 입력하세요"
+          placeholder="*이름"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
       </SignupWrapper>
 
       <SignupWrapper>
-        <Image src={signupPwd} alt="비밀번호이미지" />
+        <Image src={BackList} alt="금융기관이미지" />
         <SignUpInput
           id="financialInstitution"
           type="text"
-          placeholder="금융기관을 입력하세요"
+          placeholder="*금융기관"
+          value={financialInstitution}
+          onChange={(e) => setFinancialInstitution(e.target.value)}
         />
       </SignupWrapper>
 
       <SignupWrapper>
-        <Image src={signupPwd} alt="비밀번호이미지" />
+        <Image src={AccountNum} alt="계좌이미지" />
         <SignUpInput
           id="transactionNumber"
           type="text"
-          placeholder="거래번호를 입력하세요"
+          placeholder="*계좌번호"
+          value={transactionNumber}
+          onChange={(e) => setTransactionNumber(e.target.value)}
         />
       </SignupWrapper>
 
       <SignupWrapper>
-        <Image src={signupPwd} alt="비밀번호이미지" />
+        <Image src={Group} alt="소속이미지" />
         <SignUpInput
           id="affiliation"
           type="text"
-          placeholder="소속을 입력하세요"
+          placeholder="소속"
+          value={affiliation}
+          onChange={(e) => setAffiliation(e.target.value)}
         />
       </SignupWrapper>
 
       <SignupWrapper>
-        <Image src={signupPwd} alt="비밀번호이미지" />
-        <SignUpInput
-          id="accountNum"
-          type="text"
-          placeholder="계좌번호"
-        />
-      </SignupWrapper>
-
-      <SignupWrapper>
-        <Image src={signupPwd} alt="비밀번호이미지" />
+        <Image src={ID} alt="사업번호이미지" />
         <SignUpInput
           id="userNumber"
           type="text"
-          placeholder="사용자 번호를 입력하세요"
+          placeholder="사업자 번호"
+          value={userNumber}
+          onChange={(e) => setUserNumber(e.target.value)}
         />
       </SignupWrapper>
-
+      {showError && (
+        <ErrorMessage>＊이름, 금융기관, 거래번호는 필수 입력값입니다.</ErrorMessage>
+      )}
       <Button onClick={handleSearch}>조회</Button>
 
-      <div>
-        <p>투자자문업자이신가요?</p>
-        <LinkButton href="/투자자문업자홈">투자자문업자홈바로가기</LinkButton>
-      </div>
+    
+
+      <LinkWrapper>
+        <ThinLink href="#">투자자문업자이신가요?</ThinLink>
+        <BoldLink href="/투자자문업자홈">투자자문업자홈</BoldLink>
+      </LinkWrapper>
     </Container>
   );
 };
--- src/pages/Main/styled.js
+++ src/pages/Main/styled.js
@@ -15,21 +15,31 @@ export const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-bottom: 20px;
 `;
-
+export const LogoImage = styled.img`
+  width: 35vw;
+  height: 30vh;
+`;
 export const Image = styled.img`
-  width: 200px;
-  height: auto;
-  margin-bottom: 20px;
-`;
-
-export const Input = styled.input`
-  margin-bottom: 10px;
+  width: 10vw;
+  height: 7vh;
 `;
 
 export const Button = styled.button`
-  margin-bottom: 20px;
+  margin-bottom: 5vh;
+  background-color: #bb84e8;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  width: 82vw;
+  height: 10vh;
+`;
+
+export const LinkWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 10vh;
 `;
 
 export const LinkButton = styled.a`
@@ -37,35 +47,44 @@ export const LinkButton = styled.a`
   margin-right: 10px;
   text-decoration: none;
   color: #333;
+  font-weight: bold; /* 공통 스타일 적용 */
 `;
 
-export const ForLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: lightgray;
-  padding: 10px;
-  border-radius: 5px;
-  margin-bottom: 20px;
+export const BoldLink = styled(LinkButton)`
+  color: #6f1c7e; /* 진한 보라색 */
+`;
+
+export const ThinLink = styled(LinkButton)`
+  font-weight: normal; /* 얇은 글자 굵기 */
 `;
 
 export const SignupWrapper = styled.div`
-  width: 100%;
+  width: 80vw;
+  height: 7vh;
   display: flex;
   justify-content: space-between;
   border: 1px solid #9747ff;
   border-radius: 10px;
-  padding: 5px;
-  margin-bottom: 3vh;
+  padding: 3px;
+  margin: 1vh;
 `;
+
 export const SignUpInput = styled.input`
-  border: 1px solid rgba(0, 0, 0, 0);
-  width: 80vw;
+  border: none;
+  width: 70vw;
   border-radius: 2px;
-  height: 4vh;
+  height: 7vh;
   font-size: 0.8rem;
+  padding: 0 8px;
+  box-sizing: border-box;
   &::placeholder {
     color: #d9d9d9;
-    padding-left: 15px;
   }
+`;
+
+export const ErrorMessage = styled.div`
+  color: red;
+  width: 80vw;
+  font-size: 12px;
+  height: 8vh;
 `;
