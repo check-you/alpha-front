93c7a0d946a7bc059e58a6e40fc8d5f4ede3b5a2
kkkwp <chxrryda@gmail.com>
2023-11-19 19:26:35 +0900

    [#4] fix: 가운데 정렬 수정

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from "./Atom/Button";
 export { default as Text } from "./Atom/Text";
+export { default as HomeAppBar } from "./Atom/HomeAppBar";
 export { default as LinkedAccount } from "./Molecule/LinkedAccount";
 export { default as AddAccountBtn } from "./Molecule/AddAccountBtn";
 export { default as InputBox } from "./Molecule/InputBox";
