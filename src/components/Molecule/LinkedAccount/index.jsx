7e40c589a8e03e858f2878461b0fceff329e4d19
chososo <a50242953@gmail.com>
2023-11-20 23:30:06 +0900

    [#42] add : 계좌 삭제 api, 로직 구현

--- src/components/Molecule/LinkedAccount/index.jsx
+++ src/components/Molecule/LinkedAccount/index.jsx
@@ -9,16 +9,24 @@ import {
 import { Text } from "../../../components";
 import shinhanLogo from "../../../assets/images/shinhanLogo.svg";
 import deletbtn from "../../../assets/images/trash.svg";
-const LinkedAccount = ({ theme, onClick, children }) => {
+const LinkedAccount = ({ deleteHandler, bank, accountNum, accountName }) => {
+  /* 여기에 들어갈 내용 1. 금융기관 bank 2. 상품명 accountName 3. 계좌번호 accountNum */
+
   return (
     <Container>
-      <LogoImage src={shinhanLogo} alt="프로필 이미지 " />
+      <LogoImage src={shinhanLogo} alt="프로필 이미지" />
       <TextWrapper>
-        <Text theme="accountCorp">신한투자금융 입출금통장 [입출금]</Text>
-        <Text theme="accountNum"> 1234-567-78944 </Text>
+        <Text theme="accountCorp">
+          {bank} {accountName}신한투자금융 입출금통장 [입출금]
+        </Text>
+        <Text theme="accountNum"> 1234-567-78944{accountNum} </Text>
       </TextWrapper>
       <TrashWrapper>
-        <TrashImage src={deletbtn} alt="삭제 버튼 " />
+        <TrashImage
+          src={deletbtn}
+          alt="삭제 버튼"
+          onClick={() => deleteHandler(accountNum)}
+        />
       </TrashWrapper>
     </Container>
   );
--- src/pages/LinkedAccounts/index.jsx
+++ src/pages/LinkedAccounts/index.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { axiosInstance } from "../../apis";
+import { useNavigate } from "react-router-dom";
 import {
   Text,
   LinkedAccount,
   AddAccountBtn,
   HomeAppBar,
+  SelectModal,
 } from "../../components";
 import {
   Container,
@@ -17,6 +19,36 @@ import {
   AddBtnWrapper,
 } from "./styled";
 const LinkedAccounts = () => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [accountToDelete, setAccountToDelete] = useState(0);
+  const navigate = useNavigate();
+  const openDeleteModal = (accountN) => {
+    setAccountToDelete(accountN); // 삭제할 계좌 번호 저장
+    setIsDeleteModalOpen(true); // 모달 열기
+  };
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+  const deleteAccountApi = async (accountToDelete) => {
+    axiosInstance
+      .delete("/api/accounts", { account: accountToDelete })
+      .then((response) => {
+        console.log(response.data);
+        alert("연결된 계좌 리스트!:D");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    alert(`계좌 ${accountToDelete}가 삭제되었습니다.`); // 계좌 삭제 알림
+    setIsDeleteModalOpen(false);
+  };
+  const handleDelete = () => {
+    deleteAccountApi(accountToDelete);
+  };
+  const addAccountHandler = () => {
+    navigate("/addaccount");
+  };
+
   const getTrnasactions = async () => {
     axiosInstance
       .get("/api/accounts/yoj9168@gmail.com")
@@ -28,6 +60,7 @@ const LinkedAccounts = () => {
         console.log(e);
       });
   };
+
   ////////////////////////////////////
   // const fetchData = async () => {
   //   const accessToken = getAccessToken();
@@ -61,6 +94,16 @@ const LinkedAccounts = () => {
   }, []);
   return (
     <Container>
+      {isDeleteModalOpen && (
+        <SelectModal
+          btnContent="삭제"
+          closeHandler={closeDeleteModal}
+          okHandler={handleDelete}
+        >
+          삭제하시겠습니까?
+        </SelectModal>
+      )}
+
       <HomeAppBar label="계좌 관리" />
       <ContentWrapper>
         <TitleWrapper>
@@ -69,16 +112,18 @@ const LinkedAccounts = () => {
             <Text theme="dashBoardInfo"> 님의 계좌정보</Text>
           </NameWrapper>
           <NameWrapperbottom>
-            <Text theme="dashBoard">총 연결 계좌3 개</Text>
+            <Text theme="dashBoard">총 연결 계좌 3 개</Text>
           </NameWrapperbottom>
         </TitleWrapper>
         {/* 계좌 리스트 api로 받아서 넣기 */}
         <AccountWrapper>
-          <LinkedAccount />
+          <LinkedAccount deleteHandler={openDeleteModal} accountNum="222" />
+          <LinkedAccount deleteHandler={openDeleteModal} accountNum="223" />
+          {/* 여기에 들어갈 내용 1. 금융기관 bank 2. 상품명 accountName 3. 계좌번호 accountNum */}
         </AccountWrapper>
       </ContentWrapper>
       <AddBtnWrapper>
-        <AddAccountBtn />
+        <AddAccountBtn onClick={addAccountHandler} />
       </AddBtnWrapper>
     </Container>
   );
