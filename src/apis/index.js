53a8962a0746c88efce8bdd4127bafd20451e929
chososo <a50242953@gmail.com>
2023-11-22 08:08:18 +0900

    api 요청 주소 변경

--- src/apis/index.js
+++ src/apis/index.js
@@ -7,3 +7,11 @@ export const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// export const axiosInstance = axios.create({
+//   baseURL: `http://${process.env.REACT_APP_SERVER_PORT}`,
+//   timeout: 100000000000,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
